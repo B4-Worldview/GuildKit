26c6f04c2fcd3229624925c9f19030a5594b2b54
Nate Bate <nate@harvestmedia.com>
2022-01-17 13:06:37 -0500

    Restructure Files per JP

--- Other/LightsOut/Pods/Target Support Files/GuildKit/GuildKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GuildKit : NSObject
-@end
-@implementation PodsDummy_GuildKit
-@end
--- Other/RankedVoting/Pods/Target Support Files/Pods-RankedVoting/Pods-RankedVoting-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RankedVoting : NSObject
-@end
-@implementation PodsDummy_Pods_RankedVoting
-@end
